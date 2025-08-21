dae28eb7204f10f0798e5beacf87fe10ed1456de
Ankit Maurya <ankitmaurya0301@gmail.com>
2025-08-21 23:08:28 +0530

    python21/8

---
+++ Python1.py
@@ -0,0 +1,25 @@
+#variables 
+name = "ankit"  #string
+age = 27  #integer
+float_number = 10.5  #float
+boolean = True  #boolean
+
+print("Name:", name, "Age:", age, "Float Number:", float_number, "Boolean:", boolean)
+
+A = 10
+B = 20  
+C = 30
+print("Sum of A, B and C is", A + B + C)
+
+x = 10
+y = 40
+z = x + y
+print("Sum of x and y is", z)
+
+#calculate simple interest
+Principal = 25000
+Rate = 3.6
+Time = 5
+Simple_Interest = (Principal * Rate * Time) / 100
+print("Simple Interest is", Simple_Interest)
+
---
+++ python2.py
@@ -0,0 +1,15 @@
+# Print Type 
+x = 10
+print(type(x))      # int
+y = 3.14
+print(type(y))      # float
+z = "Hello"
+print(type(z))      # str
+flag = True
+print(type(flag))   # bool
+
+values =[12, 3.14, "Python" , True, 1000]
+
+print(values[0])
+print(values[3])
+print(values[-1])
